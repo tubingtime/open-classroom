39cebea0d4e0f76bcb6a283187ed4c10d7151bac
tj <tjtrolling@gmail.com>
2022-06-02 17:18:54 -0400

    scrollbar sync :D

--- openCR.js
+++ openCR.js
@@ -1,1 +1,9 @@
 console.log('hello');
+var topScrollbar = document.getElementById("top-scrollbar");
+var botScrollbar = document.getElementById("bot-scrollbar");
+topScrollbar.onscroll = function () {
+    botScrollbar.scrollLeft = topScrollbar.scrollLeft;
+};
+botScrollbar.onscroll = function () {
+    topScrollbar.scrollLeft = botScrollbar.scrollLeft;
+};
--- openCR.ts
+++ openCR.ts
@@ -1,1 +1,11 @@
 console.log('hello')
+let topScrollbar = document.getElementById("top-scrollbar")
+let botScrollbar = document.getElementById("bot-scrollbar")
+
+topScrollbar.onscroll = function(){
+    botScrollbar.scrollLeft = topScrollbar.scrollLeft;
+};
+
+botScrollbar.onscroll = function(){
+    topScrollbar.scrollLeft = botScrollbar.scrollLeft;
+}
