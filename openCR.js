af7a4cf666d7285e57d8ace50e18c360e039380d
tj <tjtrolling@gmail.com>
2022-06-03 16:09:23 -0400

    vite refactor

--- openCR.js
+++
@@ -1,9 +0,0 @@
-console.log('hello');
-var topScrollbar = document.getElementById("top-scrollbar");
-var botScrollbar = document.getElementById("bot-scrollbar");
-topScrollbar.onscroll = function () {
-    botScrollbar.scrollLeft = topScrollbar.scrollLeft;
-};
-botScrollbar.onscroll = function () {
-    topScrollbar.scrollLeft = botScrollbar.scrollLeft;
-};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import './style.css'
+
 console.log('hello')
 let topScrollbar = document.getElementById("top-scrollbar")
 let botScrollbar = document.getElementById("bot-scrollbar")
@@ -9,3 +11,11 @@ topScrollbar.onscroll = function(){
 botScrollbar.onscroll = function(){
     topScrollbar.scrollLeft = botScrollbar.scrollLeft;
 }
+
+function testfunc(){
+    console.log("FUCK")
+}
+let crList = document.getElementById("crList")
+let avail_row = document.getElementById("bot-scrollbar"); //prolly will need to replace id
+
+
